9a10d3778753b85111d1993e0067246b94cf05da
remitenteaswad <112367890+remitenteaswad@users.noreply.github.com>
2024-04-18 18:58:17 -0600

    Update tt_um_pwm_1.v

--- src/tt_um_pwm_1.v
+++ src/tt_um_pwm_1.v
@@ -1,28 +1,25 @@
-
 module tt_um_pwm_1 #(
   parameter width = 8
   )  (
-  input rst_n,
   input clk,
-  input [7:0] ui_in,
-  input wire ena,
-  input wire [7:0] uio_in,
-  output wire [7:0] uo_out,
-  output wire [7:0] uio_out,
-  output wire [7:0] uio_oe,
-  
-  //output wire pwm_o
+  input rst_n,
+  input [7:0]  ui_in,
+  output uo_out,
+  input  wire [7:0] uio_in,    // IOs: Input path
+  output wire [7:0] uio_out,   // IOs: Output path
+  output wire [7:0] uio_oe,    // IOs: Enable path (active high: 0=input, 1=output)
+  input wire ena
 );
 
 reg [31:0] q_reg, q_next;  // Registro para el contador del preescalado
 reg [7:0] d_reg, d_next;   // Registro para el contador del ciclo de trabajo
-reg [8:0] d_ext;           // Extensión del contador del ciclo de trabajo
-reg pwm_reg, pwm_next;     // Registro y próximo valor de la señal de PWM
-wire tick;                 // Señal para indicar el inicio de un ciclo PWM
-wire [31:0] dvsr = 32'b00000000000000000010100010110001; // Valor fijo de dvsr 104167 valor  10Mhz/960Ard
+reg [8:0] d_ext;           // Extensi n del contador del ciclo de trabajo
+reg pwm_reg, pwm_next;     // Registro y pr ximo valor de la se al de PWM
+wire tick;                 // Se al para indicar el inicio de un ciclo PWM
+wire [31:0] dvsr = 32'b00000000000000000000000000010011; // Valor fijo de dvsr 19 para clk = 10 MHz (pwm_freq = 980 Hz)
 
-always @(posedge clk, posedge rst_i) begin
-    if (rst_i) begin
+always @(posedge clk, posedge rst_n) begin
+    if (rst_n) begin
     q_reg <= 32'b0;
     d_reg <= 8'b0;
     pwm_reg <= 1'b0;
@@ -57,7 +54,7 @@ always @(*) begin
   d_ext = {1'b0, d_reg};
 end
 
-// comparison circuit (Circuito de comparación para generar PWM)
+// comparison circuit (Circuito de comparaci n para generar PWM)
 always @(*) begin
   if (d_ext < ui_in) begin
     pwm_next = 1'b1;
@@ -66,6 +63,6 @@ always @(*) begin
   end
 end
 
-  assign uo_out [0] = pwm_reg;
+assign uo_out = pwm_reg;
 
 endmodule
